4f7744b5f2ea48f8b636f17b9ede0d0750931e11
Nicolas-frnc <174860069+Nicolas-frnc@users.noreply.github.com>
2025-08-14 15:03:30 +0200

    add script to import and concatenate CSV files; rename existing dataset

---
+++ 1_data_collection/import_all_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import glob
+import os
+
+folder = "data_set"
+all_files = glob.glob(os.path.join(folder, "*.csv"))
+
+df_list = []
+for file in all_files:
+    df = pd.read_csv(file, sep=";", encoding="latin1")
+    df_list.append(df)
+
+df_all = pd.concat(df_list)
+df_all.to_csv("mix1.csv", index=False, encoding="utf-8")
+
+print("ok pour les fichiers dans le dossier :", folder )
+
